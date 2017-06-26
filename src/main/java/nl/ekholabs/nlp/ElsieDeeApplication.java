701018e66026a579d571857266c4169dbb895353
Wilder Rodrigues <wilder.rodrigues@irdeto.com>
2017-06-26 14:09:18 +0200

    Make service discoverable.

--- src/main/java/nl/ekholabs/nlp/ElsieDeeApplication.java
+++ src/main/java/nl/ekholabs/nlp/ElsieDeeApplication.java
@@ -2,8 +2,12 @@ package nl.ekholabs.nlp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.feign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients
+@EnableDiscoveryClient
 public class ElsieDeeApplication {
 
   public static void main(String[] args) {
