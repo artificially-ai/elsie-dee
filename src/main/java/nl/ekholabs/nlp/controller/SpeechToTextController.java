ef4e551ebed8f37f3a14d93bd984dce56e92abe8
Wilder Rodrigues <wilder.rodrigues@irdeto.com>
2017-06-30 10:44:01 +0200

    Update controller class and make test compliant with new code.

--- src/main/java/nl/ekholabs/nlp/controller/SpeechToTextController.java
+++ src/main/java/nl/ekholabs/nlp/controller/SpeechToTextController.java
@@ -2,6 +2,8 @@ package nl.ekholabs.nlp.controller;
 
 import java.io.IOException;
 
+import nl.ekholabs.nlp.client.ElsieDeetectFeignClient;
+import nl.ekholabs.nlp.model.Language;
 import nl.ekholabs.nlp.model.TextResponse;
 import nl.ekholabs.nlp.service.SpeechToTextService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -17,16 +19,23 @@ import static org.springframework.http.MediaType.MULTIPART_FORM_DATA_VALUE;
 public class SpeechToTextController {
 
   private final SpeechToTextService speechToTextService;
+  private final ElsieDeetectFeignClient elsieDeetect;
 
   @Autowired
-  public SpeechToTextController(final SpeechToTextService speechToTextService) {
+  public SpeechToTextController(final SpeechToTextService speechToTextService, final ElsieDeetectFeignClient elsieDeetect) {
     this.speechToTextService = speechToTextService;
+    this.elsieDeetect = elsieDeetect;
   }
 
   @PostMapping(produces = APPLICATION_JSON_UTF8_VALUE, consumes = MULTIPART_FORM_DATA_VALUE)
   public TextResponse process(final @RequestParam(value = "input") MultipartFile fileToProcess) throws IOException {
 
     final String outputText = speechToTextService.processSpeech(fileToProcess.getBytes());
-    return new TextResponse(outputText);
+
+    final Language language = elsieDeetect.identify(outputText);
+
+    return new TextResponse(language, outputText);
   }
+
+
 }
--- src/test/java/nl/ekholabs/nlp/controller/SpeechToTextControllerTest.java
+++ src/test/java/nl/ekholabs/nlp/controller/SpeechToTextControllerTest.java
@@ -2,19 +2,26 @@ package nl.ekholabs.nlp.controller;
 
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.logging.Logger;
 
+import nl.ekholabs.nlp.client.ElsieDeetectFeignClient;
+import nl.ekholabs.nlp.model.Language;
 import org.hamcrest.Matchers;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.mock.web.MockMultipartFile;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static java.lang.ClassLoader.getSystemResource;
 import static org.junit.Assert.assertThat;
+import static org.mockito.Mockito.when;
+import static org.mockito.MockitoAnnotations.initMocks;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.fileUpload;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -23,23 +30,39 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @SpringBootTest
 public class SpeechToTextControllerTest {
 
+  private static final Logger LOGGER = Logger.getLogger(SpeechToTextControllerTest.class.getName());
+
   @Autowired
   private MockMvc mvc;
+
+  @MockBean
+  private ElsieDeetectFeignClient elsieDeetect;
+
+  @Before
+  public void setup() {
+    initMocks(this);
+  }
 
   @Test
   public void processUploadedFile() throws Exception {
 
-    final String expectedResult = "{\"output\":\"<sil> the catch was crowded <sil> goes inside and out [NOISE] <sil> with "
-        + "passengers who buy that toll it seemed principally bonds to the mentions a bit \"}";
+    final String expectedResult = "{\"language\":{\"code\":\"en\"},\"output\":\"<sil> the catch was crowded <sil> goes inside and "
+        + "out [NOISE] <sil> with passengers who buy that toll it seemed principally bonds to the mentions a bit \"}";
 
     final byte[] allBytes = Files.readAllBytes(Paths.get(getSystemResource("man2_orig.wav").toURI()));
 
     final MockMultipartFile multipartFile =
         new MockMultipartFile("input", "man2_orig.wav", "audio/wav", allBytes);
 
+
+    when(elsieDeetect.identify("<sil> the catch was crowded <sil> goes inside and out [NOISE] <sil> with "
+        + "passengers who buy that toll it seemed principally bonds to the mentions a bit ")).thenReturn(new Language("en"));
+
     final String contentAsString = mvc.perform(fileUpload("/process").file(multipartFile))
         .andExpect(status().isOk())
         .andReturn().getResponse().getContentAsString();
+
+    LOGGER.info(contentAsString);
 
     assertThat(contentAsString, Matchers.notNullValue());
     assertThat(contentAsString, Matchers.equalTo(expectedResult));
