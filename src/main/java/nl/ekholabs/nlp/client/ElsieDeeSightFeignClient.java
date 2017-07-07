1bbaccadc1e11c247ecafed51b7f1f22150ebdd9
Wilder Rodrigues <wilder.rodrigues@irdeto.com>
2017-07-07 21:53:48 +0200

    Add Elsie-Dee Sight Fiegn Client.

---
+++ src/main/java/nl/ekholabs/nlp/client/ElsieDeeSightFeignClient.java
@@ -0,0 +1,42 @@
+package nl.ekholabs.nlp.client;
+
+import feign.codec.Encoder;
+import feign.form.spring.SpringFormEncoder;
+import org.springframework.cloud.netflix.feign.FeignClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.context.annotation.Scope;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestPart;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.MultipartFile;
+
+import static org.springframework.http.MediaType.APPLICATION_OCTET_STREAM_VALUE;
+
+@FeignClient(serviceId = "elsie-deesight", configuration = MultipartSupportConfig.class)
+public interface ElsieDeeSightFeignClient {
+
+  @RequestMapping(
+      value = "/elsie-deesight/process",
+      method = RequestMethod.POST,
+      consumes = MediaType.IMAGE_PNG_VALUE,
+      produces = APPLICATION_OCTET_STREAM_VALUE
+  )
+  @ResponseBody
+  ResponseEntity<byte[]> process(final @RequestPart MultipartFile image);
+}
+
+@Configuration
+class MultipartSupportConfig {
+
+  @Bean
+  @Primary
+  @Scope("prototype")
+  public Encoder feignFormEncoder() {
+    return new SpringFormEncoder();
+  }
+}
