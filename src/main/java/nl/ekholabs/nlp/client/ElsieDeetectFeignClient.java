cf678808941e4ec1ae22db278e373700c615440a
Wilder Rodrigues <wilder.rodrigues@irdeto.com>
2017-08-15 12:11:05 +0200

    Inform what the feign client consumes.

--- src/main/java/nl/ekholabs/nlp/client/ElsieDeetectFeignClient.java
+++ src/main/java/nl/ekholabs/nlp/client/ElsieDeetectFeignClient.java
@@ -6,11 +6,12 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
 import static org.springframework.http.MediaType.APPLICATION_JSON_UTF8_VALUE;
+import static org.springframework.http.MediaType.TEXT_PLAIN_VALUE;
 
 @FeignClient(serviceId = "elsie-deetect")
 public interface ElsieDeetectFeignClient {
 
-  @PostMapping(path = "elsie-deetect/identify", produces = APPLICATION_JSON_UTF8_VALUE)
+  @PostMapping(path = "elsie-deetect/identify", consumes = TEXT_PLAIN_VALUE, produces = APPLICATION_JSON_UTF8_VALUE)
   Language identify(@RequestBody final String text);
 
 }
